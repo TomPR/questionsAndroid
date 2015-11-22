f02827b652b0698c172bcea6abf29f4274d681bc
TomPR <t.planes.raisenauer@gmail.com>
2015-11-22 22:33:03 +0800

    Date time display.

--- app/src/main/java/hk/ust/cse/hunkim/questionroom/QuestionListAdapter.java
+++ app/src/main/java/hk/ust/cse/hunkim/questionroom/QuestionListAdapter.java
@@ -1,17 +1,24 @@
 package hk.ust.cse.hunkim.questionroom;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.text.Html;
+import android.text.format.DateFormat;
+import android.text.format.Time;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.firebase.client.Query;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import hk.ust.cse.hunkim.questionroom.db.DBUtil;
 import hk.ust.cse.hunkim.questionroom.question.Question;
@@ -23,6 +30,8 @@ import hk.ust.cse.hunkim.questionroom.question.Question;
  * This class is an example of how to use FirebaseListAdapter. It uses the <code>Chat</code> class to encapsulate the
  * data for each individual chat message
  */
+
+
 public class QuestionListAdapter extends FirebaseListAdapter<Question> {
 
     // The mUsername for this client. We use this to indicate which messages originated from this user
@@ -37,6 +46,8 @@ public class QuestionListAdapter extends FirebaseListAdapter<Question> {
 
         this.activity = (MainActivity) activity;
     }
+
+
 
     /**
      * Bind an instance of the <code>Chat</code> class to our view. This method is called by <code>FirebaseListAdapter</code>
@@ -77,7 +88,7 @@ public class QuestionListAdapter extends FirebaseListAdapter<Question> {
             msgString += "<font color=red>NEW </font>";
         }
 
-        msgString += "<B>" + question.getHead() + "</B>" + question.getDesc();
+        msgString += "<B>" + question.getHead() + "</B>" + question.getDesc() + "<br />" + "Posted " + getTimeAgo(question.getTimestamp()) ;
 
         ((TextView) view.findViewById(R.id.head_desc)).setText(Html.fromHtml(msgString));
         view.setOnClickListener(new View.OnClickListener() {
@@ -118,5 +129,52 @@ public class QuestionListAdapter extends FirebaseListAdapter<Question> {
     @Override
     protected void setKey(String key, Question model) {
         model.setKey(key);
+    }
+
+    private String getDate(long time) {
+        Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+        cal.setTimeInMillis(time);
+        String date = DateFormat.format("dd-MM-yyyy", cal).toString();
+        return date;
+    }
+
+    private Long getAgo(long time) {
+        Calendar cal = Calendar.getInstance(Locale.ENGLISH) ;
+        long nowTime = cal.getTimeInMillis();
+        long ago = nowTime - time ;
+        return ago ;
+    }
+
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+
+    public static String getTimeAgo(long time) {
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
+
+        Calendar cal = Calendar.getInstance(Locale.ENGLISH) ;
+        long nowTime = cal.getTimeInMillis() ;
+
+        final long diff = nowTime - time;
+        if (diff < MINUTE_MILLIS) {
+            return "just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "a minute ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " minutes ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "an hour ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " hours ago";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "yesterday";
+        } else {
+            return diff / DAY_MILLIS + " days ago";
+        }
     }
 }
